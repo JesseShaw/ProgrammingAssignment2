acb9fb1212cf5b8769d923460eeffd3dfbf050be
JesseShaw <jessecpbshaw@gmail.com>
2014-11-23 15:09:50 -0600

    Initial commit for Homework #2
    This is the first full commit of the forked ProgrammingAssignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function will:
+#Set the value of a matrix
+#Get the value of that matrix
+#Create the inverse of that matrix
+#Get the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmatrix <- function(solve) m <<- solve
+        getmatrix <- function() m
+        list(set = set, 	
+        	 get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
 }
 
+##This function will:
+#Return a matrix that is the inverse of the input 'x'
+#If a cached copy exists, return it
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=matrix(), ...) {
+        m <- x$getmatrix()
+        if(!is.null(m)){
+                message("fetching cached data")
+                return(m)
+        }
+        matrix <- x$get()
+        m <- solve(matrix, ...)
+        x$setmatrix(m)
+        m
 }
